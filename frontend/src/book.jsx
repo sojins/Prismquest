df3884b16bf62ea94e6a5e865e76dd35318a9983
Kate <sojins1976@gmail.com>
2026-07-14 22:14:16 +0900

    feat: load friend book styles

--- frontend/src/book.jsx
+++ frontend/src/book.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import BookApp from './BookApp';
 import './styles.css';
+import './book.css';
 
 createRoot(document.getElementById('root')).render(<BookApp />);
